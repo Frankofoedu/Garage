62794ae0015c0f7ed7cde5d6d806d6c1582bc452
Ofoedu Ebuka <frankofoedu@gmail.com>
2019-10-04 18:15:19 +0100

    implemented repository and unit of work interfaces

---
+++ GarageCore/Garage.Common/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Garage.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage.Common.Repositories
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity> where TEntity : class where TContext: DbContext
+    {
+        protected readonly TContext Context;
+
+        public Repository(TContext context)
+        {
+            Context = context;
+        }
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().AddRange(entities);
+        }
+
+        public async Task<IEnumerable<TEntity>>Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await Context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public TEntity Get(int id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await  Context.Set<TEntity>().ToListAsync();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            Context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
---
+++ GarageCore/Garage.Common/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Garage.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage.Common
+{
+    public sealed class UnitOfWork<TContext>: IUnitOfWork where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public UnitOfWork(TContext context)
+        {
+            _context = context;
+        }
+        public async void Dispose()
+        {
+           await _context.DisposeAsync();
+        }
+
+        public async Task<int> Save()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
